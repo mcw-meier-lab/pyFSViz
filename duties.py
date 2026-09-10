b9268efdbe174ef6e5c813e41dbd639574e89e37
Lezlie Espana <lespana@mcw.edu>
2026-09-10 14:27:09 -0500

    ci: fix file folder artifacts

--- duties.py
+++ duties.py
@@ -192,7 +192,7 @@ def release(ctx: Context, version: str = "") -> None:
 @duty(silent=True, aliases=["cov"])
 def coverage(ctx: Context) -> None:
     """Report coverage as text and HTML."""
-    ctx.run(tools.coverage.combine(), nofail=True)
+    ctx.run(tools.coverage.combine(rcfile="config/coverage.ini"), nofail=True)
     ctx.run(tools.coverage.report(rcfile="config/coverage.ini"), capture=False)
     ctx.run(tools.coverage.html(rcfile="config/coverage.ini"))
     ctx.run(tools.coverage.xml(rcfile="config/coverage.ini"))
